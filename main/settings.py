15732ad6718327a5fb8b43aeb4866f1b24770785
localeshkaa <71147169+localeshkaa@users.noreply.github.com>
2021-07-23 14:36:40 +0300

    Add files via upload

---
+++ main/settings.py
@@ -0,0 +1,16 @@
+import math
+# размер окна
+width = 1280
+height = 720
+deliver_pos = (640, 360) # место куда везти пиво
+FPS = 60
+start = True
+
+#цвета
+YELLOW = (253, 165, 15)
+GREY = (128, 128, 128)
+WHITE = (255, 255, 255)
+GREEN = (123, 208, 103)
+DARK_GREEN = (0, 100, 0)
+RED = (255, 0, 0)
+BLACK = (0, 0, 0)
